3819c7b5530841369cbbc486b827b0e48e71e4c2
Giancarlo Garcia <giancarlo3g@gmail.com>
2025-09-17 21:00:38 +0000

    NID added with TWAMP originator

--- save_config.sh
+++ save_config.sh
@@ -7,10 +7,10 @@ fi
 
 
 yes | cp ./clab-twamp/ce/A/config/cf3/config.cfg ./config/ce.cfg
+yes | cp ./clab-twamp/nid/A/config/cf3/config.cfg ./config/nid.cfg
 yes | cp ./clab-twamp/cpe/A/config/cf3/config.cfg ./config/cpe.cfg
 yes | cp ./clab-twamp/agg/A/config/cf3/config.cfg ./config/agg.cfg
 yes | cp ./clab-twamp/spe/A/config/cf3/config.cfg ./config/spe.cfg
 yes | cp ./clab-twamp/coi-l2/A/config/cf3/config.cfg ./config/coi-l2.cfg
 yes | cp ./clab-twamp/coi-l3/A/config/cf3/config.cfg ./config/coi-l3.cfg
 yes | cp ./clab-twamp/peering/A/config/cf3/config.cfg ./config/peering.cfg
-yes | cp ./clab-twamp/originator/A/config/cf3/config.cfg ./config/originator.cfg
